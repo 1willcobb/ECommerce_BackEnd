33a1b054fbb563a74a01c8a0657d981908668c39
1willcobb <cobb.will@gmail.com>
2023-08-06 20:10:57 -0700

    started category-routes.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,57 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// find all categories including its associated Products
+router.get('/', async (req, res) => {
+  try {
+    
+
+    res.status(200).send("Success")
+  } catch (error) {
+    console.log(err);
+    res.status(500).send("Server Error")
+  }
 });
 
+// find one category by its `id` value including its associated Products
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+
+  try {
+    res.status(200).send("Success")
+  } catch (error) {
+    console.log(err);
+    res.status(500).send("Server Error")
+  }
 });
 
+// create a new category
 router.post('/', (req, res) => {
-  // create a new category
+  try {
+    res.status(200).send("Success")
+  } catch (error) {
+    console.log(err);
+    res.status(500).send("Server Error")
+  }
 });
 
+// update a category by its `id` value
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  try {
+    res.status(200).send("Success")
+  } catch (error) {
+    console.log(err);
+    res.status(500).send("Server Error")
+  }
 });
 
+// delete a category by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  try {
+    res.status(200).send("Success")
+  } catch (error) {
+    console.log(err);
+    res.status(500).send("Server Error")
+  }
 });
 
 module.exports = router;
